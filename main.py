c9ab6ccbd192a9ac88898eb20cceb537cb21c35f
Ian Pickering <ipickering2@gmail.com>
2016-01-30 19:57:54 -0600

    add log.txt endpoint

--- main.py
+++ main.py
@@ -1,14 +1,79 @@
-from flask import Flask
+# all the imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash, Response
+from contextlib import closing
+from datetime import datetime
+
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+DATABASE = '/tmp/elona.db'
+
+def connect_to_database():
+    return sqlite3.connect(DATABASE)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+        db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        for i in range(0,50):
+            db.execute('insert into chat (time, kind, name, descr, text, addr) values (?, ?, ?, ?, ?, ?)',
+                     [datetime.now().strftime("%s"), 1, "弱気ものprin", "は猫に殺された", "なむ", "127.0.0.1"])
+        db.commit()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+def type(x):
+    return {
+        0: 'chat',
+        1: 'dead',
+        2: 'wish',
+    }.get(x, 0)
 
 @app.route("/text.txt")
 def text():
-    return app.send_static_file('text.txt')
+    response = app.send_static_file('text.txt')
+    return response
 
 
-@app.route("/vote.txt")
+@app.route("/log.txt")
 def vote():
-    return app.send_static_file('vote.txt')
+    response = ""
+    first = query_db('select * from chat order by id desc limit 1', one=True)
+    no = first['id']+1 if first else 1
+    response += str(no) + "<C>\n<!--START-->\n"
+    for line in query_db('select * from chat order by id desc limit 30'):
+        date = datetime.fromtimestamp(line['time']).strftime("%m/%d(%I)")
+        response += str(line['id']) + '%' + date + '%' + type(line['kind']) + line['name'] + line['descr'] + '「' + line['text'] + '」%' + line['addr'] + '%\n'
+    response += "<!--END-->\n<!-- WebTalk v1.6 --><center><small><a href='http://www.kent-web.com/' target='_top'>WebTalk</a></small></center>"
+    return Response(response, mimetype='text/plain')
+    
 
 if __name__ == "__main__":
     app.run()
--- schema.sql
+++ schema.sql
@@ -3,22 +3,24 @@ begin transaction;
 -- log(en).txt format:
 -- 7685733%1/04(10)%dead淑女の精霊サリーは盗賊の隠れ家で強盗に殺された。「今日は眠れないな」%1.95.255.124.ap.yournet.ne.jp%
 
+drop table if exists chat;
 create table chat (
   id integer primary key autoincrement,
   time int not null,     -- UNIX timestamp to be converted to [M/DD(HH)]
-  kind tinyint not null, -- one of "dead" (0), "chat" (1) or "wish" (2)
+  kind tinyint not null, -- one of "chat" (0), "dead" (1) or "wish" (2)
   name text not null,    -- name of player
   descr text not null,   -- description of message
   text text not null,    -- player-provided text of message
   addr text not null     -- IP address/website of player
 );
 
-create index chat_idx on chat(name,time)
+create index chat_idx on chat(name,time);
 
 -- vote(en).txt format:
 -- 1<>十字の蛇scar<>53<>114.157.208.48<>1449840576#625#66#<>
 -- (first digit is line no.)
 
+drop table if exists vote;
 create table vote (
   name text primary key,       -- name of registrant
   votes integer not null,      -- number of votes
